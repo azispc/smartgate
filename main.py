5bfcea556fa9ff0042f3acee80aa51cee1759324
Moh.Azis <aziscs20@gmail.com>
2019-11-08 00:42:30 +0700

    update

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import face_recognition as citra
+import paho.mqtt.client as mqtt
+import requests
+import pymongo
+import json
+import fungsi_publish as pub
 
-#fungsi panggil fungsi 
-while True:
-    hasil=input("masukkan jarak:")
-    if(float(hasil)>10):
-        print("\n Jarak Memenuhi Syarat")
-        citra.fungsirecognetion()
+BROKER_ADDRESS = '192.168.43.201'
+MQTT_PORT = 1883
+MQTT_TOPIC = 'jarak1'
+
+def on_connect(client, userdata, flags, rc):
+    print('Connected with result code',  str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, message):
+    payload = message.payload.decode('utf-8')
+    print(payload)
+    jarak=float(payload)
+    if(jarak<30):
+        print("panggil fungsi recog")
+        statusservo=citra.fungsirecognetion()
+        print(statusservo)
+        client.connect(BROKER_ADDRESS, MQTT_PORT, 60)
+        print("publishing message to topic")
+        client.publish("servo",statusservo)
     else:
-        # Do a bit of cleanup
-        print("\n Jarak Tidak Memenuhi Syarat")
+        print("tidak ada orang")
+
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(BROKER_ADDRESS, MQTT_PORT, 60)
+    client.loop_forever()
